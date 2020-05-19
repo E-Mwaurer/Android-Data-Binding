daada9bb6565413c7851b3c0b079888d92750eab
E-Mwaurer <edduemwaurer@outlook.com>
2020-05-19 07:20:26 +0300

    Update DictionaryFragment.kt
    Update DictionaryFragment.kt to support views with data binding

--- ui/DictionaryFragment.kt
+++ ui/DictionaryFragment.kt
@@ -6,38 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
 
 class DictionaryFragment : Fragment() {
     
-    private lateinit var wordEditText: EditText
-    private lateinit var wordMeaningEditText: EditText
-    private lateinit var submitBtn: Button
-    private lateinit var outputText: TextView
-    
+    private lateinit var defaultWord: DomainWord
+ 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val root = inflater.inflate(R.layout.dictionary-layout,container,false)
+        val binding = DatabindingUtil.inflate<DictionaryLayoutBinding>(
+            inflater,
+            R.layout.dictionary-layout,
+            container,
+            false
+        )
 
-        // making ui framework calls to get hold of views on the layout
+        defaultWord = DomainWord("","")
         
-        wordEditText = root.findViewById<EditText>(R.id.word_text)
-        wordMeaningEditText = froot.indViewById<EditText>(R.id.word_meaning)
-        submitBtn = root.findViewById<Button>(R.id.btnSubmit)
-        outputText = root.findViewById<TextView>(R.id.wordsWritten)
-                
-        submitBtn.setOnClickListener{
-            // populate the output textview with values that have been input from the editTexts
-            outputText.text = resources.getString(
-                R.string.word_meaning_output,
-                wordEditText.text.toString(),
-                wordMeaningEditText.text.toString()
-            )
+        binding.let{
+            lifecycleOwner = viewLifecycleOwner
+            word = defaultWord
         }
         
-        return root
+        return binding.root
     }
     
 }
